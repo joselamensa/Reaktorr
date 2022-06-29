ce4b9de4e12f0d3bca4d854e6945f668fa3a9c54
joselamensa <joselamensa10@gmail.com>
2022-06-29 19:11:52 -0300

    agrego funciones de js

--- java/js.js
+++ java/js.js
@@ -1,3 +1,19 @@
 function agregar() {
     document.getElementById("agregado").innerHTML = " Añadido 1 unidad";
 }
+
+function validar()
+{
+    var usuario = document.getElementById("usuario").value;
+    var Contraseña = document.getElementById("pass").value;	
+
+    if(usuario == "jose" && Contraseña == "1234")
+    {
+        window.location= "./personaldata.html";
+    }
+    else
+    {
+        document.getElementById("incorrec").innerHTML = "Contraseña incorrecta";
+    }
+
+}
