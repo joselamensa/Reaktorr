4b9aac99595570fd3c80128b6dc82a62b3de57f3
joselamensa <joselamensa10@gmail.com>
2022-07-06 20:49:46 -0300

    pruebo cerrar sesión

--- java/js.js
+++ java/js.js
@@ -203,6 +203,6 @@ function onSignIn(googleUser) {
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        console.log('User signed out.');
     });
+    auth2.disconnect();
   }
