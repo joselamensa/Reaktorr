0bf43777e35d8a73810ea07afa258aae7782a799
joselamensa <joselamensa10@gmail.com>
2022-07-25 22:45:41 -0300

    agrego calculadora y emprolijo

--- java/js.js
+++ java/js.js
@@ -216,3 +216,94 @@ function signOut() {
     });
     auth2.disconnect();
 }
+
+
+// Calculadora pag oculta
+
+//Funciones on click de numeros
+uno.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "1";
+};
+dos.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "2";
+};
+tres.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "3";
+};
+cuatro.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "4";
+};
+cinco.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "5";
+};
+seis.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "6";
+};
+siete.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "7";
+};
+ocho.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "8";
+};
+nueve.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "9";
+};
+cero.onclick = function (e) {
+  resultado.textContent = resultado.textContent + "0";
+};
+reset.onclick = function (e) {
+  resetear();
+};
+suma.onclick = function (e) {
+  operandoa = resultado.textContent;
+  operacion = "+";
+  limpiar();
+};
+resta.onclick = function (e) {
+  operandoa = resultado.textContent;
+  operacion = "-";
+  limpiar();
+};
+multiplicacion.onclick = function (e) {
+  operandoa = resultado.textContent;
+  operacion = "*";
+  limpiar();
+};
+division.onclick = function (e) {
+  operandoa = resultado.textContent;
+  operacion = "/";
+  limpiar();
+};
+igual.onclick = function (e) {
+  operandob = resultado.textContent;
+  resolver();
+};
+function limpiar() {
+  resultado.textContent = "";
+}
+function resetear() {
+  resultado.textContent = "";
+  operandoa = 0;
+  operandob = 0;
+  operacion = "";
+}
+
+function resolver() {
+  var res = 0;
+  switch (operacion) {
+    case "+":
+      res = parseFloat(operandoa) + parseFloat(operandob);
+      break;
+    case "-":
+      res = parseFloat(operandoa) - parseFloat(operandob);
+      break;
+    case "*":
+      res = parseFloat(operandoa) * parseFloat(operandob);
+      break;
+    case "/":
+      res = parseFloat(operandoa) / parseFloat(operandob);
+      break;
+  }
+  resetear();
+  resultado.textContent = res;
+}
